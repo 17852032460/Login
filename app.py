735c04697b77765cb02a488b21072a9fcc0fc5fb
17852034260 <1914636379@qq.com>
2019-02-22 22:10:55 +0800

    增加配置文件，练习URL翻转

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+#encoding: utf-8
+
+from flask import Flask, url_for, redirect
+import config
 
 app = Flask(__name__)
-
+app.config.from_object(config)
 
 @app.route('/')
-def hello_world():
+def index():
+    print url_for('article', id = 123)
     return 'Hello World!'
 
+@app.route('/article/<id>')
+def article(id):
+    return id
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
