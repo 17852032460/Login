947750f004f1641d40fa1e5127251d0aebb1c5c6
17852034260 <1914636379@qq.com>
2019-02-25 21:50:59 +0800

    注册界面

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ app.config.from_object(config)
 def index():
     return render_template('index.html')
 
-@app.route('/login/')
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/register/')
+@app.route('/register/', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
 
-@app.route('/user_register/')
+@app.route('/user_register', methods=['GET', 'POST'])
 def user_register():
+    print u'hello world!'
     db = pymysql.connect('localhost', 'root', '236326', 'myproj', 3306)
     cursor = db.cursor()
     sql = "INSERT INTO users VALUES('%s', '%s') % (request.from.get('id'), request.from.get('password'))"
