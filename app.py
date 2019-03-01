86ba2eb368a4d0d8acd54a835f5523e678e99b5a
17852034260 <1914636379@qq.com>
2019-03-01 20:45:50 +0800

    完成注册登录功能，增加注册用户名重复等错误提示页面

--- app.py
+++ app.py
@@ -18,29 +18,47 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/wrong/<be>', methods=['GET', 'POST'])
+def wrong(be):
+    if be == '1':
+        return render_template("wrong1.html")
+    elif be == '2':
+        return render_template("wrong2.html")
+    elif be == '3':
+        return render_template("wrong3.html")
+
 @app.route('/user_register/', methods=['GET', 'POST'])
 def user_register():
-    get_id = request.form['id']
+    get_username = request.form['username']
+    get_nickname = request.form['nickname']
     get_password = request.form['password']
     get_repassword = request.form['repassword']
     if get_password == get_repassword:
         db = pymysql.connect(host = 'localhost', port = 3306, user = 'root', password = '236326', db = 'myproj', charset = 'utf8')
         cursor = db.cursor()
-        sql = "INSERT INTO users(id, password) VALUES('%s', '%s')" % (get_id, get_password)
-        cursor.execute(sql)
-        db.commit()
-        db.close()
-        return redirect('/login/')
+        sql_query = "SELECT username FROM users where username = '%s'" % get_username
+        sql_insert = "INSERT INTO users(username, nickname, password) VALUES('%s', '%s', '%s')" % (get_username, get_nickname, get_password)
+        cursor.execute(sql_query)
+        judgeUsername = cursor.fetchone()
+        if judgeUsername != None:
+            db.commit()
+            db.close()
+            return redirect('/wrong/3')
+        else:
+            cursor.execute(sql_insert)
+            db.commit()
+            db.close()
+            return render_template('successlogin.html')
     else:
-        return redirect('/register/')
+        return redirect('/wrong/2')
 
 @app.route('/user_login/', methods=['GET', 'POST'])
 def user_login():
-    get_id = request.form['id']
+    get_username = request.form['username']
     get_password = request.form['password']
     db = pymysql.connect(host='localhost', port=3306, user='root', password='236326', db='myproj', charset='utf8')
     cursor = db.cursor()
-    sql = "SELECT password FROM users where id = '%s'" % get_id
+    sql = "SELECT password FROM users where username = '%s'" % get_username
     cursor.execute(sql)
     password = cursor.fetchone()
     db.commit()
@@ -48,11 +66,11 @@ def user_login():
     if password[0] == get_password:
         return redirect('/query/')
     else:
-        return redirect('/login/')
+        return redirect('/wrong/1')
 
 @app.route('/query/', methods=['GET', 'POST'])
 def question():
-    return render_template('quert.html')
+    return render_template('query.html')
 
 if __name__ == '__main__':
     app.run()
